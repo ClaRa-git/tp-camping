c6a84f4a65b8f6d0a1b1e4fab134651da958f3aa
ClaRa-git <rameau.claire.cr@gmail.com>
2025-02-19 22:25:51 +0100

    Retrait des use inutiles

--- www/src/Controller/ReservationController.php
+++ www/src/Controller/ReservationController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class ReservationController extends AbstractController
 {
